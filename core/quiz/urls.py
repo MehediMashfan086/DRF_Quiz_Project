e3d51ddbc320791f377bb8d6239859760d179bd1
MehediMashfan086 <mehedihasan62086@gmail.com>
2022-03-23 10:44:43 +0600

    3rd commit

--- core/quiz/urls.py
+++ core/quiz/urls.py
@@ -5,5 +5,7 @@ from .views import *
 app_name = 'quiz'
 
 urlpatterns = [
-    path('', Quiz.as_view(), name='quiz')
+    path('', Quiz.as_view(), name='quiz'),
+    path('r/<str:topic>/', RandomQuestion.as_view(), name = 'random'),
+    path('q/<str:topic>/', QuizQuestion.as_view(), name = 'questions')
 ]
